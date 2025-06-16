beca2460bb309a7984a846252bd620ab736034bf
Rizky Ramadan <ramadanrizky593@gmail.com>
2025-06-16 13:47:03 +0700

    add role on auth

--- backend/auth-service/models/user.js
+++ backend/auth-service/models/user.js
@@ -1,10 +1,45 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  email: { type: String },
-});
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: false, // kosong jika OAuth
+    },
+    role: {
+      type: String,
+      enum: ["admin", "employee"],
+      required: true,
+    },
+    username: {
+      type: String,
+      required: false,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      required: false,
+    },
+    oauth: {
+      provider: {
+        type: String,
+        required: false,
+      },
+      providerId: {
+        type: String,
+        required: false,
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
