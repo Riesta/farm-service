669ffabe70ea7b236bb8cabfa68403d5b06eb88e
Your Name <you@example.com>
2025-06-16 12:54:16 +0700

    tambah user.js buat get id user

--- backend/auth-service/routes/auth.js
+++ backend/auth-service/routes/auth.js
@@ -11,7 +11,6 @@ const generateToken = (user) => {
   return jwt.sign({ id: user._id }, jwtSecret, { expiresIn: jwtExpiresIn });
 };
 
-// // Contoh di auth-service
 // router.get("/:id", async (req, res) => {
 //   const user = await User.findById(req.params.id);
 //   if (!user) return res.status(404).json({ message: "User tidak ditemukan" });
